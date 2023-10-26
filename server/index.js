3ba8dc40d703c12112ec12efc51ae6ef3f645be0
m-camacho <mario.camacho2@ymail.com>
2023-10-25 23:04:46 -0400

    Very minor change in message format

--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
+const http = require("http").createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)}: ${message}`);
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
+http.listen(8080, () => console.log("listening on http://localhost:8080"));
 
 // Regular Websockets
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -31,6 +29,3 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 //   });
 
 // });
-
-
- 
